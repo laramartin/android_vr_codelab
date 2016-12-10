1d2c52264bf5cd10d3e7f44686e23b854b47c49a
Lara Martin <lara.martin.carretero@gmail.com>
2016-12-10 13:50:56 +0100

    :sparkles: initialize member variables

--- app/src/main/java/com/google/devrel/vrviewapp/GorillaFragment.java
+++ app/src/main/java/com/google/devrel/vrviewapp/GorillaFragment.java
@@ -20,6 +20,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.vr.sdk.widgets.video.VrVideoView;
 
 /**
  * Fragment for the Gorilla tab.
@@ -27,9 +31,46 @@ import android.view.ViewGroup;
 public class GorillaFragment extends Fragment {
     private static final String TAG = "GorillaFragment";
 
+    /**
+     * Preserve the video's state and duration when rotating the phone. This improves
+     * performance when rotating or reloading the video.
+     */
+    private static final String STATE_IS_PAUSED = "isPaused";
+    private static final String STATE_VIDEO_DURATION = "videoDuration";
+    private static final String STATE_PROGRESS_TIME = "progressTime";
+
+
+    /**
+     * The video view and its custom UI elements.
+     */
+    private VrVideoView videoWidgetView;
+
+    /**
+     * Seeking UI & progress indicator. The seekBar's progress value represents milliseconds in the
+     * video.
+     */
+    private SeekBar seekBar;
+    private TextView statusText;
+
+    /**
+     * By default, the video will start playing as soon as it is loaded.
+     */
+    private boolean isPaused = false;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.gorilla_fragment, container,false);
+        View view = inflater.inflate(R.layout.gorilla_fragment, container,false);
+        seekBar = (SeekBar) view.findViewById(R.id.seek_bar);
+        statusText = (TextView) view.findViewById(R.id.status_text);
+        videoWidgetView = (VrVideoView) view.findViewById(R.id.video_view);
+
+        // Add the restore state code here.
+
+        // Add the seekbar listener here.
+
+        // Add the VrVideoView listener here
+
+        return view;
     }
 }
